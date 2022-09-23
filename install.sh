2322a1c758487d2834d0c7cad3459a76039c33bf
Manjinder Singh Tiwana <81515337+Ante-Boy@users.noreply.github.com>
2022-09-23 18:53:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -65,7 +65,7 @@ mkdir ~/m4qlo
 cd ~/m4qlo
 git clone https://github.com/dotfighter/torshammer.git
 git clone http://github.com/cyweb/hammer
-git clone https://github.com/Ante-Boy/Xerxes.git
+https://github.com/Ante-Boy/Xerxes.git
 cd ~/m4qlo/xerxes
 gcc xerxes.c -o xerxes
 cd ~/
